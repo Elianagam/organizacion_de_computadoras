78e5d82d14fade7447c2ead5f05c8466db8ef030
Tomás <fedee962@outlook.com>
2019-03-22 16:41:46 -0300

    ultimo

--- conversor.c
+++ conversor.c
@@ -4,18 +4,38 @@
 int main(){
 
 	FILE* archivoDeMierda = fopen("pruebaWindows.txt", "r");
-	char leido[30];
+	char caracter;
+	char* buffer = malloc(200 * sizeof(char));
 
-	if(archivoDeMierda == NULL)
-		printf("No se abrio");
+	if(archivoDeMierda == NULL){
+		fclose(archivoDeMierda);
+		return 0;
+	}
+	
+	int i = 0;
 
+	//Windows a linux : borrar el \r
 	while(!feof(archivoDeMierda)){
 
-		fread(leido, sizeof(char), 1, archivoDeMierda); 
-		printf("Se leyo: %s \n", leido);
+		fread(&caracter, sizeof(char), 1, archivoDeMierda);
+
+		if (caracter != '\r'){
+			buffer[i] = caracter;
+			i++;	
+
+		}else{
+			printf("un bufer: %s \n", buffer);
+			i = 0;
+			for(int j = 0; j < 200; j++)
+				buffer[j] = ' ';
+		}
 	}
 
+	printf("Se leyo: %s \n", buffer);
+
 	fclose(archivoDeMierda);
+	//free(buffer);
 
 	return 0;
 }
+
