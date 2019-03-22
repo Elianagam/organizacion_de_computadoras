a6504ec07764ca0bf61d3f61083e9cac2798d56e
Tomás <fedee962@outlook.com>
2019-03-22 17:43:18 -0300

    bergas de Leonardo

--- linuxToWindows.c
+++ linuxToWindows.c
@@ -1,22 +1,23 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#define  CONST_R
+const char CONST_R = '\r';
 
 //Agregar \r antes de cada \n
 
 int main(){
 
 	FILE* f_in = fopen("linuxToWindowsTest.txt", "r");
-	FILE* f_out = fopen("out_w.txt", "w+");
+	FILE* f_out = fopen("out_w.txt", "w");
 
 	char r_char;
+	char const_r = '\r';
 
-	while(!feof(file)){
+	while(!feof(f_in)){
 
-		fread(&r_char, sizeof(char), 1, file);
+		fread(&r_char, sizeof(char), 1, f_in);
 		if(r_char == '\n')
-			fwrite(CONST_R, 1, sizeof(char), f_out);
+			fwrite(&const_r, 1, sizeof(char), f_out);
 
 		fwrite(&r_char, 1, sizeof(char), f_out);
 	}
