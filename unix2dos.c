242aafd4c07ce41c6893da7effbc41e1791f3487
Tomás <fedee962@outlook.com>
2019-03-27 22:12:45 -0300

    Dos programas completos, faltan pruebas

--- unix2dos.c
+++ unix2dos.c
@@ -1,25 +1,25 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
 
-const char CONST_R = '\r';
+const char CONST_CR = '\r';
+const char CONST_NL = '\n';
 
 //Agregar \r antes de cada \n
 
 int main(int argsc, char* argv[]){
 
 	FILE* f_in = fopen(argv[1], "r");
-	//Si rompe lanzar exception y cerrar archivo
+  if(!f_in)
+    fclose(f_in);
+
 	FILE* f_out = fopen("out_w.txt", "w");
 
 	char r_char;
-	char const_r = '\r';
 
 	while(!feof(f_in)){
 
 		fread(&r_char, sizeof(char), 1, f_in);
-		if(r_char == '\n')
-			fwrite(&const_r, 1, sizeof(char), f_out);
+		if(r_char == CONST_NL)
+			fwrite(&CONST_CR, 1, sizeof(char), f_out);
 
 		fwrite(&r_char, 1, sizeof(char), f_out);
 	}
