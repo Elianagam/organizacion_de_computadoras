0ef7b3b285864c2777379c76dbc3cf0fc4544c23
Eliana <elianagam2@gmail.com>
2019-04-21 13:14:14 -0300

    modifico dos2unix

--- tp1/dos2unix.c
+++ tp1/dos2unix.c
@@ -1,21 +1,23 @@
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
 #include "read.h"
 #include "write.h"
 
-#define max_size 2000
+#define MAX_SIZE 2000
 
 int main(int argsc, char* argv[]){
-	// unix2dos
-	FILE* f_in = fopen(argv[1], "r");
-  	if(!f_in) return 0;
+    // dos2unix
+    FILE* f_in = fopen(argv[1], "r");
+    if(!f_in) return 0;
 	
-	char buffer[MAX_SIZE];
-	buffer = read_dos_file(f_in, buffer);
+    char* buffer = malloc(sizeof(char) * MAX_SIZE);
+    buffer = read_dos_file(f_in, buffer);
 	
-	char file_out[80] = "out_";
-	strcat(file_out, argv[1]);
+    char file_out[80] = "out_";
+    strcat(file_out, argv[1]);
 	
-	write_file(file_out, buffer);
-	return 0;
+    write_file(file_out, buffer);
+    free(buffer);
+    return 0;
 }
