937572684943693974e96ec4d65d598e24d70aae
Leonardo Bellaera <leobellaera@gmail.com>
2019-04-29 14:40:09 -0300

    Update pruebas_automaticas_unix2dos.sh

--- tp1/pruebas_automaticas_unix2dos.sh
+++ tp1/pruebas_automaticas_unix2dos.sh
@@ -1,15 +1,11 @@
 #!/usr/pkg/bin/bash
 
-
-
-
-
   echo Running dos2unix testings...
         i=1
 	while [ "$i" -le $1 ]; do
-    		./unix2dos <test_dos2unix_$i.txt >temp.a.$i.txt || break
+    		./unix2dos <test_unix2dos_$i.txt >temp.a.$i.txt || break
     		./dos2unix <temp.a.$i.txt >temp.b.$i.txt || break
-    		diff -q test_dos2unix_$i.txt temp.b.$i.txt || break
+    		diff -q test_unix2dos_$i.txt temp.b.$i.txt || break
     		echo Ok: $i;
     		rm -f temp.*
                 i=$(( i + 1 ))
